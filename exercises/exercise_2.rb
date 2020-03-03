1dd61a8a94bbfff57b395c8a8bd188a51f86c77d
Flavian <Flavianmohanraj@gmail.com>
2020-03-03 03:00:43 +0000

    exercise 3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,8 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
-@store1 = Store.find_by(id:1)
-@store2 = Store.find_by(id:2)
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
 puts "Store 1 is: #{@store1.inspect}"
 puts "Store 2 is: #{@store2.inspect}"
-store1.update(name: "Vancouver Island")
+@store1.update(name: "Vancouver Island")
